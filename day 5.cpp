2348ef7fc17e6a784832c0473973b9debaa0d93a
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2022-11-20 22:18:30 +0530

    Add files via upload

---
+++ day 5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+   int n;
+   cout<<"enter any no. = ";
+   cin>>n;
+   if(n%2==0)
+   {
+      cout<<"Even number";
+   }
+   else
+   {
+      cout<<"Odd number";
+   }
+}
